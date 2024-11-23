29fb146a7a14972f7018f3e8b9696d37ceee45e0
Raven Ling <tree12132002@hotmail.com>
2024-11-24 02:43:46 +0800

    import public and tailwind extend setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500">Welcome to Awwwards</h1>
+      <h1 className="text-5xl font-bold text-orange-500">
+        Welcome to Awwwards
+      </h1>
     </main>
   )
 }
