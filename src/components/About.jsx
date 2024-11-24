ff9eefbc9324aa41f6ca69df1edeaf8df2a27e12
tree12132002 <tree12132002@hotmail.com>
2024-11-25 01:49:28 +0800

    feat: create animated title

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,7 +32,10 @@ const About = () => {
           Welcome to Zentry
         </h2>
 
-        <AnimatedTitle />
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's l<b>a</b>rgest shared adventure"
+          containerClass="mt-5 !text-black text-center"
+        />
 
         <div className="about-subtext">
           <p>The Game of Games begins-your life, now an epic MMORPG</p>
